8452e8d75ee072a7d4077448400a97e1a848f313
신윤호 <xode114kr1@naver.com>
2024-09-05 21:54:11 +0900

    feat : 라우터 셋팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ProductAll from "./page/ProductAll";
+import Login from "./page/Login";
+import ProductDetail from "./page/ProductDetail";
+// 1. 전체 상품 페이지, 로그인 페이지, 상품상세페이지
+// 2. 전체 상품 페이지 : 전체 상품을 볼수 있음
+// 3. 로그인 버튼을 누르면 로그인 페이지로 이동
+// 4. 상품 디테일을 눌렀으나 로그인이 안되있을 시, 로그인 페이지로 이동
+// 5. 로그인이 되어 잇을 경우 상품상세 페이지로 이동
+// 6. 상품 상세 페이지에서 로그아웃 시, 로그아웃이 되고 로그인 페이지로 이동
+// 7. 로그인 상태 시 로그아웃/ 로그아웃 시 로그인으로 표시
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="product/:id" element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/page/ProductAll.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductAll = () => {
+  return <div>상품</div>;
+};
+
+export default ProductAll;

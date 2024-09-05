bdeb6770f73517b580ad52d9b914eb658461598f
신윤호 <xode114kr1@naver.com>
2024-09-05 22:43:32 +0900

    feat : 상단 메뉴 만들기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
 import ProductDetail from "./page/ProductDetail";
+import Navbar from "./component/Navbar";
 // 1. 전체 상품 페이지, 로그인 페이지, 상품상세페이지
 // 2. 전체 상품 페이지 : 전체 상품을 볼수 있음
 // 3. 로그인 버튼을 누르면 로그인 페이지로 이동
@@ -14,6 +15,7 @@ import ProductDetail from "./page/ProductDetail";
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
         <Route path="/login" element={<Login />} />

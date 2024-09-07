ac8e918d981bcd05ec6a3dbe038e611cb0568eb2
신윤호 <xode114kr1@naver.com>
2024-09-07 22:44:32 +0900

    feat : login form 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
 import ProductDetail from "./page/ProductDetail";
 import Navbar from "./component/Navbar";
 import "./App.css";
+import { useState } from "react";
+
 // 1. 전체 상품 페이지, 로그인 페이지, 상품상세페이지
 // 2. 전체 상품 페이지 : 전체 상품을 볼수 있음
 // 3. 로그인 버튼을 누르면 로그인 페이지로 이동
@@ -15,6 +16,7 @@ import "./App.css";
 // 7. 로그인 상태 시 로그아웃/ 로그아웃 시 로그인으로 표시
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false);
   return (
     <div>
       <Navbar />

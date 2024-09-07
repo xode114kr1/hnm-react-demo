119b4a5194911eb18e5c36515b20b6f073dd34cc
신윤호 <xode114kr1@naver.com>
2024-09-07 19:48:24 +0900

    feat : API호출 및 ProductCard prototype 생성

---
+++ src/component/ProductCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img
+        width="300px"
+        src="https://noona-hnm.netlify.app/pattern-jacket.jpeg"
+      />
+      <div>Count choices</div>
+      <div>title</div>
+      <div>price</div>
+      <div>new</div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../component/ProductCard";
 
 const ProductAll = () => {
-  return <div>상품</div>;
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = "http://localhost:4000/products";
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
 };
 
 export default ProductAll;

e9f84973718ab6de9fdf92586ff4859bd7a784db
신윤호 <xode114kr1@naver.com>
2024-09-08 19:37:00 +0900

    feat : 상품 디테일 페이지

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const ProductDetail = () => {
-  return <div>상품상세페이지</div>;
+  const props = useParams();
+  const [product, setProduct] = useState(null);
+  const getProductDetail = async () => {
+    let url = `http://localhost:4000/products/${props.id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className="product-detail-img">
+            <img width="300px" src={product?.img} />
+          </Col>
+          <Col className="product-detail-word">
+            <div>{product?.title}</div>
+            <div>{product?.price}</div>
+            <div>{product && product.choice ? "Conscious choice" : " "}</div>
+            <Form.Select
+              aria-label="Default select example"
+              className="product-detail-select-size"
+            >
+              {product &&
+                product.size.map((ops, index) => (
+                  <option value={index}>{ops}</option>
+                ))}
+            </Form.Select>
+            <Button className="product-detail-add-button" variant="dark">
+              추가
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductDetail;
